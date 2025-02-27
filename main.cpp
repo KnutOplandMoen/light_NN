57398c71b9ebc7a94522ae23c9499de72d2f1f2a
NicolaiBaklund <baklundnicolai@gmail.com>
2025-02-27 21:47:32 +0100

    Implement Matrix class with random value initialization and activation function application + basic functionality for matrices

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "functions.h"
+#include "matrix.h"
 #include <iostream>
-using namespace std;
+
 
 int main() {
-    cout << test_function() << endl;
-    cout << "heija" << endl;
+    Matrix m(5,5);
+    std::cout << m;
+    m.setRandomValues(-5.0, 5.0);
+    std::cout << m;
+    std::cout << m.applyActivationFunction("sigmoid");
     return 0;
 }
