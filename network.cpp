87f79c5d3638d7ae29ebe1db2c4edcad1dcb4b4a
KnutOplandMoen <knut.o.moen@gmail.com>
2025-02-27 21:11:21 +0100

    Implement weight initialization for neural network layers

--- network.cpp
+++ network.cpp
@@ -2,6 +2,14 @@
 #include <vector>
 #include "network.h"
 
+/**
+ * Initialise the weights for the neural network layers.
+ * 
+ * This function creates and initializes the weight matrices for the neural network.
+ * The weights are initialized between the input layer and the first hidden layer,
+ * between each pair of consecutive hidden layers, and between the last hidden layer
+ * and the output layer.
+ */
 std::vector <Matrix> network::initialise_weights(Matrix input_layer, std::vector <Matrix> hidden_layers, Matrix output_layer) {
     std::vector <Matrix> weights; 
     weights.push_back(Matrix(hidden_layers[0].getRows(), input_layer.getRows())); 
@@ -9,5 +17,7 @@ std::vector <Matrix> network::initialise_weights(Matrix input_layer, std::vector
         weights.push_back(Matrix(hidden_layers[i+1].getRows(), hidden_layers[i].getRows()));
     }
     weights.push_back(Matrix(output_layer.getRows(), hidden_layers.back().getRows()));
+
+    this -> weights = weights;
 }
 
