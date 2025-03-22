c188de7d7be0158b2b42286f8d0ac2f92f35639a
KnutOplandMoen <knut.o.moen@gmail.com>
2025-03-22 22:59:14 +0100

    Refactor feed forward visualization

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@
 
 int main() {
     // Define the sizes for input, hidden layers, and output layers
-    std::vector<int> hidden_layers_sizes = {5, 20};  // hidden layers and neurons in each layer
+    std::vector<int> hidden_layers_sizes = {10, 10};  // hidden layers and neurons in each layer
     Matrix output_layer(11, 1); // Output layer with 11 neurons
     Matrix input_layer = input_to_matrix({0, 0, 0, 0}); // inout layer with 4 neurons
 
-    std::vector <std::string> activation_functions = {"leakyReLu", "leakyReLu", "softmax"}; //activation and output functions, should match be of dim: (1 + number of hidden layers)
+    std::vector <std::string> activation_functions = {"reLu", "reLu", "softmax"}; //activation and output functions, should match be of dim: (1 + number of hidden layers)
 
     // Initialize the network with the layers
     network nn(input_layer, hidden_layers_sizes, output_layer, activation_functions);
-    //nn.load_state("abcx_copy.txt"); // Load the state (weights and biases) from file
+    //nn.load_state("abcx_model.txt"); // Load the state (weights and biases) from file
 
     /* 
     // Get the data
@@ -37,12 +37,13 @@ int main() {
     // Train the network
     nn.train(x_labels_train, y_labels_train, x_labels_test, y_labels_test, epochs, learning_rate, batch_size, true);
     */
-    // Test the network on a single input
-    std::vector<std::vector<Matrix>> prediction = nn.feed_forward_pass(input_to_matrix({3, 2, 3, 1}));
-    std::cout << "Prediction: \n" << prediction[0].back() << std::endl;
 
-    nn.visualise_network(true); 
-    visualize_feed_forward(prediction[0], input_to_matrix({3, 2, 3, 1})); // Visualize the feed forward pass
+    // Test the network on a single input
+    Matrix input = input_to_matrix({0, 6, 3, 1}); // Input to the network
+    std::vector<std::vector<Matrix>> prediction = nn.feed_forward_pass(input); // Feed forward pass
+    std::cout << "Prediction: \n" << prediction[0].back() << std::endl; // Print the prediction
+    visualize_feed_forward(prediction[0], input); // Visualize the feed forward pass with AnimationWindow
+
     nn.save_state("abcx_copy.txt"); // Save the weights and viases to a file in binary format
     return 0;
 }
