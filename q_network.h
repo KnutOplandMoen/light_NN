bc8d6bd260cbc62f535a421d842927d13a2b12a9
KnutOplandMoen <knut.o.moen@gmail.com>
2025-04-02 22:06:32 +0200

    Refactor main function and q_network class for improved architecture and performance; update layer sizes and enhance information retrieval

--- q_network.h
+++ q_network.h
@@ -11,9 +11,11 @@ struct information {
     int done;
     double q_target_value;
     Matrix q_target;
+    std::vector<Matrix> activated_layers;
     information(Matrix q_values, double q_value, double reward, int done, double q_target_value, Matrix q_target, Matrix state)
         : q_values(q_values), q_value(q_value), reward(reward), done(done), 
           q_target_value(q_target_value), q_target(q_target), state(state) {}
+    information() {}
 };
 
 class q_network : public network {
@@ -25,7 +27,7 @@ class q_network : public network {
     double total_reward = 0;
     public:
     q_network(int input_layer_size, std::vector <int> hidden_layers_sizes, int output_layer_size, std::vector <std::string> activationFuncions) : network(input_layer_size, hidden_layers_sizes, output_layer_size, activationFuncions) {};
-    information get_information(Matrix& input, Game& game_play);
+    information get_information(Matrix& input, Game& game_play, bool nextState = true);
     double reward(Matrix current_state);
     void update_net(double learning_rate, int mini_batch_size, std::deque<information> experiences);
     void train(int games, int batch_size, int mini_batch_size, double learning_rate, std::map<std::string, int> autosave_file = {});
